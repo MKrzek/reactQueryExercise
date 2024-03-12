e3206fd76c8662d0825fb375777b3523e8e17108
mkrzekTDS <malgorzata.krzek@testdrivensolutions.co.uk>
2024-03-12 16:58:07 +0000

    feat: implement react query

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,43 @@
-import { Link } from "react-router-dom";
 
+import { useQuery } from 'react-query';
+import {GoIssueOpened, GoIssueClosed} from 'react-icons/go'
+
+function IssueItem (props){
+    const {
+        number,
+        assignee,
+        commentCount,
+        key,
+        title,
+        status
+    } = props
+    return <li>
+        <div>
+            {status==='done'|| status=='cancel'}
+        </div>
+    </li>
+}
 export default function IssuesList() {
+
+    const issuesQ = useQuery(['issues'], ()=>fetch('/api/issues').then(res=>res.json))
+
+
   return (
     <div>
-      <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
+      <h2>Issues List</h2>
+     { issuesQ.isLoading
+     ? <p>Loading...</p>
+     :<ul>
+        {issuesQ.data.map(issue=><IssueItem
+        number={issue.number}
+        assignee={issue.assignee}
+        commentCount={issue.comments.length}
+        key={issue.id}
+        title={issue.title}
+        {...issue}
+         />)}
       </ul>
+}
     </div>
   );
 }
